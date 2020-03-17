ee286c05baf76c7562a6b774bffc7637202485be
Touyama Hiroki <touyamahiroki@Hirokis-MacBook-Pro.local>
2020-03-17 18:01:51 +0900

    first commit

---
+++ Pods/Target Support Files/Pods-VerticalLabelSample/Pods-VerticalLabelSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VerticalLabelSample : NSObject
+@end
+@implementation PodsDummy_Pods_VerticalLabelSample
+@end
---
+++ VerticalLabelSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VerticalLabelSample
+//
+//  Created by 藤山裕輝 on 2020/03/17.
+//  Copyright © 2020 藤山裕輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
